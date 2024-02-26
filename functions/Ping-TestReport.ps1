143382259c6a2f18c4d114d19bb3247d30bfa3c3
albddnbn <105134673+albddnbn@users.noreply.github.com>
2024-02-26 15:18:21 -0500

    Update Ping-TestReport.ps1
    ping test corrected - not using begin/process/end approach quite yet.

--- functions/Ping-TestReport.ps1
+++ functions/Ping-TestReport.ps1
@@ -41,149 +41,144 @@ function Ping-TestReport {
         $PingCount,
         $Outputfile
     )
-    BEGIN {
-        $thedate = Get-Date -Format 'yyyy-MM-dd'
+    # BEGIN {
+    $thedate = Get-Date -Format 'yyyy-MM-dd'
 
-        ## TARGETCOMPUTER HANDLING:
-        ## If Targetcomputer is an array or arraylist - it's already been sorted out.
-        if (($TargetComputer -is [System.Collections.IEnumerable]) -and ($TargetComputer -isnot [string])) {
-            $null
-            ## If it's a string - check for commas, try to get-content, then try to ping.
+    ## TARGETCOMPUTER HANDLING:
+    ## If Targetcomputer is an array or arraylist - it's already been sorted out.
+    if (($TargetComputer -is [System.Collections.IEnumerable]) -and ($TargetComputer -isnot [string])) {
+        $null
+        ## If it's a string - check for commas, try to get-content, then try to ping.
+    }
+    elseif ($TargetComputer -is [string]) {
+        if ($TargetComputer -in @('', '127.0.0.1')) {
+            $TargetComputer = @('127.0.0.1')
         }
-        elseif ($TargetComputer -is [string]) {
-            if ($TargetComputer -in @('', '127.0.0.1')) {
-                $TargetComputer = @('127.0.0.1')
-            }
-            elseif ($Targetcomputer -like "*,*") {
-                $TargetComputer = $TargetComputer -split ','
-            }
-            elseif (Test-Path $Targetcomputer -erroraction SilentlyContinue) {
-                $TargetComputer = Get-Content $TargetComputer
+        elseif ($Targetcomputer -like "*,*") {
+            $TargetComputer = $TargetComputer -split ','
+        }
+        elseif (Test-Path $Targetcomputer -erroraction SilentlyContinue) {
+            $TargetComputer = Get-Content $TargetComputer
+        }
+        else {
+            $test_ping = Test-Connection -ComputerName $TargetComputer -count 1 -Quiet
+            if ($test_ping) {
+                $TargetComputer = @($TargetComputer)
             }
             else {
-                $test_ping = Test-Connection -ComputerName $TargetComputer -count 1 -Quiet
-                if ($test_ping) {
-                    $TargetComputer = @($TargetComputer)
-                }
-                else {
-                    $TargetComputerInput = $TargetComputerInput + "x"
-                    $TargetComputerInput = Get-ADComputer -Filter * | Where-Object { $_.DNSHostname -match "^$TargetComputerInput*" } | Select -Exp DNShostname
-                    $TargetComputerInput = $TargetComputerInput | Sort-Object   
-                }
+                $TargetComputerInput = $TargetComputerInput + "x"
+                $TargetComputerInput = Get-ADComputer -Filter * | Where-Object { $_.DNSHostname -match "^$TargetComputerInput*" } | Select -Exp DNShostname
+                $TargetComputerInput = $TargetComputerInput | Sort-Object   
             }
         }
-        $TargetComputer = $TargetComputer | Where-object { $_ -ne $null }
-        # Safety catch to make sure
-        if ($null -eq $TargetComputer) {
-            # user said to end function:
-            return
-        }
-
-        # get the hour/min of day in filestring
-        if ($([int](Get-Date -Format 'HH')) -le 11) {
-            $am_pm = "AM"
-        }
-        else {
-            $am_pm = "PM"
-        }
-
-        ## If the user didn't submit outputfile value - use default
-        if ($outputfile -eq '') {
-            $REPORT_TITLE = "PingTest-$(Get-Date -Format 'hh-MM')$($am_pm)"
-        }
-        ## If they submitted a value that wasn't 'n' - use that value to create filepath
-        elseif ($outputfile.ToLower() -ne 'n') {
-            $REPORT_TITLE = "PingTest-$outputfile-$(Get-Date -Format 'hh-MM')$($am_pm)"
-            # if ($Outputfile -eq '') { $outputfile = $REPORT_TITLE } elseif ($outputfile.ToLower() -ne 'n') {
-        }
-
-        ## If Get-OutputFileString is available, use it to create outputfile string
-        if (Get-Command -Name 'Get-OutputFileString' -ErrorAction SilentlyContinue) {
-            $outputfile = Get-OutputFileString -Titlestring $REPORT_TITLE -rootdirectory $env:PSMENU_DIR -foldertitle $REPORT_TITLE -reportoutput
-        }
-        ## Otherwise - just assign filename to be report title
-        else {
-            $outputfile = $REPORT_TITLE
-        }
-
-        $results_container = [system.collections.arraylist]::new()
-
-        $PingCount = [int]$PingCount
     }
+    $TargetComputer = $TargetComputer | Where-object { $_ -ne $null }
+    # Safety catch to make sure
+    if ($null -eq $TargetComputer) {
+        # user said to end function:
+        return
+    }
+
+    # get the hour/min of day in filestring
+    if ($([int](Get-Date -Format 'HH')) -le 11) {
+        $am_pm = "AM"
+    }
+    else {
+        $am_pm = "PM"
+    }
+
+    ## If the user didn't submit outputfile value - use default
+    if ($outputfile -eq '') {
+        $REPORT_TITLE = "PingTest-$(Get-Date -Format 'hh-MM')$($am_pm)"
+    }
+    ## If they submitted a value that wasn't 'n' - use that value to create filepath
+    elseif ($outputfile.ToLower() -ne 'n') {
+        $REPORT_TITLE = "PingTest-$outputfile-$(Get-Date -Format 'hh-MM')$($am_pm)"
+        # if ($Outputfile -eq '') { $outputfile = $REPORT_TITLE } elseif ($outputfile.ToLower() -ne 'n') {
+    }
+
+    ## If Get-OutputFileString is available, use it to create outputfile string
+    if (Get-Command -Name 'Get-OutputFileString' -ErrorAction SilentlyContinue) {
+        $outputfile = Get-OutputFileString -Titlestring $REPORT_TITLE -rootdirectory $env:PSMENU_DIR -foldertitle $REPORT_TITLE -reportoutput
+    }
+    ## Otherwise - just assign filename to be report title
+    else {
+        $outputfile = $REPORT_TITLE
+    }
+
+    $results_container = [system.collections.arraylist]::new()
+
+    $PingCount = [int]$PingCount
+    # }
 
     ## Ping EACH Target computer / record results into ps object, add to arraylist (results_container)
-    PROCESS {
-        ForEach ($single_computer in $TargetComputer) {
-            $obj = [pscustomobject]@{
-                Sourcecomputer       = $env:COMPUTERNAME
-                ComputerHostName     = $single_computer
-                TotalPings           = $pingcount
-                Responses            = 0
-                AvgResponseTime      = 0
-                PacketLossPercentage = 0
-            }
-            for ($i = 0; $i -lt $pingcount; $i++) {
-                # $ping = Test-NetConnection -ComputerName $single_computer -Count 1 -ErrorAction SilentlyContinue
-                # $details = $ping.pingreplydetails
-                # $triptime = $details.roundtriptime
-                $ping = Test-Connection -ComputerName $single_computer -Count 1 -ErrorAction SilentlyContinue
-                if ($ping) {
-                    $obj.Responses += 1
-                    $intresponsetime = [int]$ping.ResponseTime
-                    $obj.AvgResponseTime += $intresponsetime
-                }
-            }
-            # avg response time
-            if ($obj.Responses -eq 0) {
-
-                $obj.AvgResponseTime = 0
-            }
-            else {
-
-                $obj.AvgResponseTime = $($obj.AvgResponseTime) / $($obj.Responses)
-            }
-
-            # calculate packet loss percentage - divide total pings by responses
-            $total_drops = $obj.TotalPings - $obj.Responses
-            $obj.PacketLossPercentage = ($total_drops / $($obj.TotalPings)) * 100
-
-            $results_container.add($obj) | Out-Null
+    # PROCESS {
+    ForEach ($single_computer in $TargetComputer) {
+        # read-host "beginning loop"
+        $obj = [pscustomobject]@{
+            Sourcecomputer       = $env:COMPUTERNAME
+            ComputerHostName     = $single_computer
+            TotalPings           = $pingcount
+            Responses            = 0
+            AvgResponseTime      = 0
+            PacketLossPercentage = 0
         }
+
+        $results = Test-Connection -ComputerName $single_computer -count $PingCount -ErrorAction SilentlyContinue
+
+        $obj.responses = $results.count
+
+        $sum_of_response_times = $($results | measure-object responsetime -sum)
+
+
+
+        $obj.avgresponsetime = $sum_of_response_times.sum / $obj.responses
+        if ($obj.Responses -eq 0) {
+
+            $obj.AvgResponseTime = 0
+        }
+
+        # calculate packet loss percentage - divide total pings by responses
+        $total_drops = $obj.TotalPings - $obj.Responses
+        $obj.PacketLossPercentage = ($total_drops / $($obj.TotalPings)) * 100
+
+        $results_container.add($obj) | Out-Null
     }
+    # }
 
     ## Report file creation or terminal output
-    END {
+    # END {
 
-        if ($outputfile.tolower() -eq 'n') {
-            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Detected 'N' input for outputfile, skipping creation of outputfile."
-            # $results_container | format-table -autosize
-            $results_container | out-gridview
+    if ($outputfile.tolower() -eq 'n') {
+        Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Detected 'N' input for outputfile, skipping creation of outputfile."
+        # $results_container | format-table -autosize
+        $results_container | out-gridview
+    }
+    else {
+        if (Get-Command -name 'output-reports' -erroraction SilentlyContinue) {
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Exporting " -nonewline
+            Write-Host "$outputfile" -foregroundcolor green -NoNewline
+            Write-Host " to .csv/.xlsx."
+    
+            Output-Reports -Filepath "$outputfile" -Content $results_container -ReportTitle "Ping Test $thedate" -CSVFile $true -XLSXFile $true
+    
+            # /-- Open in File explorer
+            Invoke-Item "$env:PSMENU_DIR\reports\$thedate\$REPORT_TITLE\"
         }
         else {
-            if (Get-Command -name 'output-reports' -erroraction SilentlyContinue) {
-                Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Exporting " -nonewline
-                Write-Host "$outputfile" -foregroundcolor green -NoNewline
-                Write-Host " to .csv/.xlsx."
-    
-                Output-Reports -Filepath "$outputfile" -Content $results_container -ReportTitle "Ping Test $thedate" -CSVFile $true -XLSXFile $true
-    
-                # /-- Open in File explorer
-                Invoke-Item "$env:PSMENU_DIR\reports\$thedate\$REPORT_TITLE\"
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Function was not run as part of Terminal Menu - does not have utility functions." -Foregroundcolor Yellow
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Exporting " -nonewline
+            Write-Host "$outputfile" -foregroundcolor green -NoNewline
+            Write-Host " to .csv only"
+            $results_container | Export-Csv -Path "$outputfile.csv" -NoTypeInformation
+            # /-- Open in File explorer
+            try {
+                Invoke-Item "$outputfile.csv"
             }
-            else {
-                Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Function was not run as part of Terminal Menu - does not have utility functions." -Foregroundcolor Yellow
-                Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Exporting " -nonewline
-                Write-Host "$outputfile" -foregroundcolor green -NoNewline
-                Write-Host " to .csv only"
-                $results_container | Export-Csv -Path "$outputfile.csv" -NoTypeInformation
-                # /-- Open in File explorer
-                try {
-                    Invoke-Item "$outputfile.csv"
-                }
-                catch {
-                    Write-Host "Failed to open $outputfile.csv."
-                }
+            catch {
+                Write-Host "Failed to open $outputfile.csv."
             }
         }
     }
+    # }
 }
