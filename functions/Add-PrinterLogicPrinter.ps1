60d40748fc039df4ecc9786a470533d5e776ece2
albddnbn <105134673+albddnbn@users.noreply.github.com>
2024-02-29 13:08:23 -0500

    Update Add-PrinterLogicPrinter.ps1

--- functions/Add-PrinterLogicPrinter.ps1
+++ functions/Add-PrinterLogicPrinter.ps1
@@ -31,7 +31,7 @@ function Add-PrinterLogicPrinter {
         Author: albddnbn (Alex B.)
         Project Site: https://github.com/albddnbn/PSTerminalMenu
     #>
-    # [CmdletBinding()]
+    [CmdletBinding()]
     param (
         [Parameter(
             Mandatory = $true,
@@ -40,119 +40,200 @@ function Add-PrinterLogicPrinter {
         $TargetComputer,
         [string]$PrinterName
     )
-    # BEGIN {
-    $thedate = Get-Date -Format 'yyyy-MM-dd'
-    ## TARGETCOMPUTER HANDLING:
-    ## If Targetcomputer is an array or arraylist - it's already been sorted out.
-    if (($TargetComputer -is [System.Collections.IEnumerable]) -and ($TargetComputer -isnot [string])) {
-        $null
-        ## If it's a string - check for commas, try to get-content, then try to ping.
-    }
-    elseif ($TargetComputer -is [string]) {
-        if ($TargetComputer -in @('', '127.0.0.1')) {
-            $TargetComputer = @('127.0.0.1')
-        }
-        elseif ($Targetcomputer -like "*,*") {
-            $TargetComputer = $TargetComputer -split ','
-        }
-        elseif (Test-Path $Targetcomputer -erroraction SilentlyContinue) {
-            $TargetComputer = Get-Content $TargetComputer
+    ## 1. TargetComputer handling if not supplied through pipeline
+    ## 2. Outputfile path creation
+    ## 3. Scriptblock to connect to printerlogic printer
+    ## 4. Results containers for overall results and skipped computers.
+    BEGIN {
+        $thedate = Get-Date -Format 'yyyy-MM-dd'
+        ## 1. Handle TargetComputer input if not supplied through pipeline (will be $null in BEGIN if so)
+        if ($null -eq $TargetComputer) {
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Detected pipeline for targetcomputer." -Foregroundcolor Yellow
         }
         else {
-            $test_ping = Test-Connection -ComputerName $TargetComputer -count 1 -Quiet
-            if ($test_ping) {
-                $TargetComputer = @($TargetComputer)
+            if (($TargetComputer -is [System.Collections.IEnumerable]) -and ($TargetComputer -isnot [string])) {
+                $null
+                ## If it's a string - check for commas, try to get-content, then try to ping.
+            }
+            elseif ($TargetComputer -is [string]) {
+                if ($TargetComputer -in @('', '127.0.0.1')) {
+                    $TargetComputer = @('127.0.0.1')
+                }
+                elseif ($Targetcomputer -like "*,*") {
+                    $TargetComputer = $TargetComputer -split ','
+                }
+                elseif (Test-Path $Targetcomputer -erroraction SilentlyContinue) {
+                    $TargetComputer = Get-Content $TargetComputer
+                }
+                else {
+                    $test_ping = Test-Connection -ComputerName $TargetComputer -count 1 -Quiet
+                    if ($test_ping) {
+                        $TargetComputer = @($TargetComputer)
+                    }
+                    else {
+                        $TargetComputerInput = $TargetComputerInput + "x"
+                        $TargetComputerInput = Get-ADComputer -Filter * | Where-Object { $_.DNSHostname -match "^$TargetComputerInput*" } | Select -Exp DNShostname
+                        $TargetComputerInput = $TargetComputerInput | Sort-Object   
+                    }
+                }
+            }
+            $TargetComputer = $TargetComputer | Where-object { $_ -ne $null }
+            # Safety catch
+            if ($null -eq $TargetComputer) {
+                return
+            }
+        }
+
+        ## 2. Outputfile handling - either create default, create filenames using input, or skip creation if $outputfile = 'n'.
+        $str_title_var = "PrtLogicConnect"
+        if ($Outputfile.tolower() -eq 'n') {
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Detected 'N' input for outputfile, skipping creation of outputfile."
+        }
+        else {
+            if (Get-Command -Name "Get-OutputFileString" -ErrorAction SilentlyContinue) {
+                if ($Outputfile.toLower() -eq '') {
+                    $REPORT_DIRECTORY = "$str_title_var"
+                }
+                else {
+                    $REPORT_DIRECTORY = $outputfile            
+                }
+                $OutputFile = Get-OutputFileString -TitleString $REPORT_DIRECTORY -Rootdirectory $env:PSMENU_DIR -FolderTitle $REPORT_DIRECTORY -ReportOutput
             }
             else {
-                $TargetComputerInput = $TargetComputerInput + "x"
-                $TargetComputerInput = Get-ADComputer -Filter * | Where-Object { $_.DNSHostname -match "^$TargetComputerInput*" } | Select -Exp DNShostname
-                $TargetComputerInput = $TargetComputerInput | Sort-Object   
+                Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Function was not run as part of Terminal Menu - does not have utility functions." -Foregroundcolor Yellow
+                if ($outputfile.tolower() -eq '') {
+                    $iterator_var = 0
+                    while ($true) {
+                        $outputfile = "$env:PSMENU_DIR\reports\$thedate\$REPORT_DIRECTORY\$str_title_var-$thedate"
+                        if ((Test-Path "$outputfile.csv") -or (Test-Path "$outputfile.xlsx")) {
+                            $iterator_var++
+                            $outputfile = "$env:PSMENU_DIR\reports\$thedate\$REPORT_DIRECTORY\$str_title_var-$([string]$iterator_var)"
+                        }
+                        else {
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        ## 3. Define the scriptblock that connects machine to target printer in printerlogic cloud instance
+        $connect_to_printer_block = {
+            param(
+                $printer_name
+            )
+
+            $obj = [pscustomobject]@{
+                hostname       = $env:COMPUTERNAME
+                printer        = $printer_name
+                connectstatus  = 'NO'
+                clientsoftware = 'NO'
+            }
+            # get installerconsole.exe
+            $exepath = get-childitem -path "C:\Program Files (x86)\Printer Properties Pro\Printer Installer Client\bin" -Filter "PrinterInstallerConsole.exe" -File -Erroraction SilentlyContinue
+            if (-not $exepath) {
+                Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$env:COMPUTERNAME] :: PrinterLogic PrinterInstallerConsole.exe was not found in C:\Program Files (x86)\Printer Properties Pro\Printer Installer Client\bin." -Foregroundcolor Red
+                return $obj
+            }
+        
+            $obj.clientsoftware = 'YES'
+        
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$env:COMPUTERNAME] :: Found $($exepath.fullname), mapping $printer_name now..."
+            $map_result = (Start-Process "$($exepath.fullname)" -Argumentlist "InstallPrinter=$printer_name" -Wait -Passthru).ExitCode
+        
+            # 0 = good, 1 = bad
+            if ($map_result -eq 0) {
+                Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$env:COMPUTERNAME] :: Connected to $printer_name successfully." -Foregroundcolor Green
+                # Write-Host "*Remember that this script does not set default printer, user has to do that themselves."
+                $obj.connectstatus = 'YES'
+                return $obj
+            }
+            else {
+                Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$env:COMPUTERNAME] :: failed to connect to $printer_name." -Foregroundcolor Red
+                return $obj
+            }
+        }
+
+        ## 4. Create empty results containers
+        $results = [system.collections.arraylist]::new()
+        $missed_computers = [system.collections.arraylist]::new()
+    }
+    ## 1. Make sure no $null or empty values are submitted to the ping test or scriptblock execution.
+    ## 2. Ping the single target computer one time as test before attempting remote session.
+    ## 3. If machine was responsive, run scriptblock to attempt connection to printerlogic printer, save results to object
+    PROCESS {
+        ## 1.
+        if ($TargetComputer) {
+            # may be able to remove the next 3 lines.
+            if ($Targetcomputer -eq '127.0.0.1') {
+                $TargetComputer = $env:COMPUTERNAME
+            }
+            ## 2. test with ping:
+            $pingreply = Test-Connection $TargetComputer -Count 1 -Quiet
+            if ($pingreply) {
+                ## 3.
+                $printer_connection_results = Invoke-Command -ComputerName $TargetComputer -scriptblock $connect_to_printer_block -ArgumentList $PrinterName
+            
+                $results.add($printer_connection_results)
+            }
+            else {
+                Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: $TargetComputer is not responding to ping." -Foregroundcolor Red
+                $missed_computers.Add($TargetComputer)
             }
         }
     }
-    $TargetComputer = $TargetComputer | Where-object { $_ -ne $null }
-    # Safety catch to make sure
-    if ($null -eq $TargetComputer) {
-        # user said to end function:
-        return
-    }
-    
-    $connect_to_printer_block = {
-        param(
-            $printer_name
-        )
+    ## 1. If there are any results - output to file
+    ## 2. Output missed computers to terminal.
+    END {
+        ## 1.
+        if ($results) {
 
-        $obj = [pscustomobject]@{
-            hostname       = $env:COMPUTERNAME
-            printer        = $printer_name
-            connectstatus  = 'NO'
-            clientsoftware = 'NO'
-        }
-        # get installerconsole.exe
-        $exepath = get-childitem -path "C:\Program Files (x86)\Printer Properties Pro\Printer Installer Client\bin" -Filter "PrinterInstallerConsole.exe" -File -Erroraction SilentlyContinue
-        if (-not $exepath) {
-            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$env:COMPUTERNAME] :: PrinterLogic PrinterInstallerConsole.exe was not found in C:\Program Files (x86)\Printer Properties Pro\Printer Installer Client\bin." -Foregroundcolor Red
-            return $obj
-        }
-        
-        $obj.clientsoftware = 'YES'
-        
-        Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$env:COMPUTERNAME] :: Found $($exepath.fullname), mapping $printer_name now..."
-        $map_result = (Start-Process "$($exepath.fullname)" -Argumentlist "InstallPrinter=$printer_name" -Wait -Passthru).ExitCode
-        
-        # 0 = good, 1 = bad
-        if ($map_result -eq 0) {
-            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$env:COMPUTERNAME] :: Connected to $printer_name successfully." -Foregroundcolor Green
-            # Write-Host "*Remember that this script does not set default printer, user has to do that themselves."
-            $obj.connectstatus = 'YES'
-            return $obj
+            ## Output results list to file:
+            ## Sort the results
+            if ($outputfile.tolower() -eq 'n') {
+                # Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Detected 'N' input for outputfile, skipping creation of outputfile."
+                if ($results.count -le 2) {
+                    $results | Format-List
+                    # $results | Out-GridView
+                }
+                else {
+                    $results | out-gridview
+                }
+            }
+            else {
+                $results | Export-Csv -Path "$outputfile.csv" -NoTypeInformation
+                ## Try ImportExcel
+                try {
+                    ## xlsx attempt:
+                    $params = @{
+                        AutoSize             = $true
+                        TitleBackgroundColor = 'Blue'
+                        TableName            = "$REPORT_DIRECTORY"
+                        TableStyle           = 'Medium9' # => Here you can chosse the Style you like the most
+                        BoldTopRow           = $true
+                        WorksheetName        = "$REPORT_DIRECTORY"
+                        PassThru             = $true
+                        Path                 = "$Outputfile.xlsx" # => Define where to save it here!
+                    }
+                    $Content = Import-Csv "$Outputfile.csv"
+                    $xlsx = $Content | Export-Excel @params
+                    $ws = $xlsx.Workbook.Worksheets[$params.Worksheetname]
+                    $ws.View.ShowGridLines = $false # => This will hide the GridLines on your file
+                    Close-ExcelPackage $xlsx
+                }
+                catch {
+                    Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: ImportExcel module not found, skipping xlsx creation." -Foregroundcolor Yellow
+                }
+                Invoke-item "$($outputfile | split-path -Parent)"
+            }
         }
         else {
-            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] [$env:COMPUTERNAME] :: failed to connect to $printer_name." -Foregroundcolor Red
-            return $obj
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: No results to output."
         }
-    }
-
-    Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Connecting printer: " -NoNewLine
-    Write-Host "$printername" -Foregroundcolor Yellow
-    Write-Host "targetcomputer value : $TargetComputer" -foregroundcolor cyan
-    # }
-    # PROCESS {
-    if ($TargetComputer -eq '127.0.0.1') {
-        $results = Invoke-Command -Scriptblock $connect_to_printer_block -ArgumentList $PrinterName
-        $results | add-member -MemberType NoteProperty -Name 'PSComputerName' -Value $env:COMPUTERNAME
-    }
-    else {
-        $results = Invoke-Command -ComputerName $TargetComputer -scriptblock $connect_to_printer_block -ArgumentList $PrinterName
-    }
-    $need_software_installed = ($results | where-object { $_.clientsoftware -eq 'NO' }).PSComputerName
-
-    $failed_connections = ($results | Where-Object { $_.connectstatus -eq 'NO' }).PSComputerName
-    # no need to repeat the ones that need software installed
-    $failed_connections = $failed_connections | where-object { $_ -notin $($need_software_installed) }
-
-    if ($need_software_installed) {
-        Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: " -NoNewline
-        Write-Host "These computers " -NoNewline
-        Write-Host "need PrinterLogic software installed" -Foregroundcolor Red -NoNewline
-        Write-Host "."
-        # Write-Host "$($need_software_installed -join ', ')"
-        $need_software_installed
+        Write-Host "These computers did not respond to ping:"
         Write-Host ""
-    }
-
-    if ($failed_connections) {
-        Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: " -NoNewline
-        Write-Host "These computers " -NoNewline
-        Write-Host "failed to connect to $printername" -Foregroundcolor Red -NoNewline
-        Write-Host ", but have Printer Logic software installed."
+        $missed_computers
         Write-Host ""
-        # Write-Host "$($failed_connections -join ', ')" -Foregroundcolor red
-        $failed_connections
+        Read-Host "Press enter to return results."
+        return $results
     }
-    # }
-    # END {
-
-    Read-Host "Press enter to continue."
-    # }
 }
