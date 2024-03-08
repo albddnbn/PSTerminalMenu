b1dd1b3b437867eb2c63f2ed164d318ba951ef17
Alex B <105134673+albddnbn@users.noreply.github.com>
2024-03-08 08:58:56 -0500

    Adding List Dot Net versions function
    Function is able to be executed through menu and presents a gridview of .NET installations on each target computer in the list. I was able to create this function by pasting in the repeatable code sections:
    Begin block - Targetcomputer processing
    PRocess block - Testing each TargetComputer to see if it's responsive on network, then running the scriptblock (in this case - creating an object for each dotnet version found on each computer).
    Outputs to Gridview, and I'm hoping to eventually just have one row for each computer, listing all dotnet versions to make it a bit easier to read.

---
+++ functions/List-DotNets.ps1
@@ -0,0 +1,120 @@
+Function List-DotNets {
+    param(
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            Position = 0
+        )]
+        [String[]]$TargetComputer
+    )
+    BEGIN {
+        $thedate = Get-Date -Format 'yyyy-MM-dd'
+        ## 1. Handle TargetComputer input if not supplied through pipeline (will be $null in BEGIN if so)
+        if ($null -eq $TargetComputer) {
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Detected pipeline for targetcomputer." -Foregroundcolor Yellow
+        }
+        else {
+            if (($TargetComputer -is [System.Collections.IEnumerable]) -and ($TargetComputer -isnot [string[]])) {
+                $null
+                ## If it's a string - check for commas, try to get-content, then try to ping.
+            }
+            elseif ($TargetComputer -is [string[]]) {
+                if ($TargetComputer -in @('', '127.0.0.1')) {
+                    $TargetComputer = @('127.0.0.1')
+                }
+                elseif ($Targetcomputer -like "*,*") {
+                    $TargetComputer = $TargetComputer -split ','
+                }
+                elseif (Test-Path $Targetcomputer -erroraction SilentlyContinue) {
+                    $TargetComputer = Get-Content $TargetComputer
+                }
+                else {
+                    ## CREDITS FOR The code this was adapted from: https://intunedrivemapping.azurewebsites.net/DriveMapping
+                    if ([string]::IsNullOrEmpty($env:USERDNSDOMAIN) -and [string]::IsNullOrEmpty($searchRoot)) {
+                        Write-Error "LDAP query `$env:USERDNSDOMAIN is not available!"
+                        Write-Warning "You can override your AD Domain in the `$overrideUserDnsDomain variable"
+                    }
+                    else {
+
+                        # if no domain specified fallback to PowerShell environment variable
+                        if ([string]::IsNullOrEmpty($searchRoot)) {
+                            $searchRoot = $env:USERDNSDOMAIN
+                        }
+
+                        $searcher = New-Object -TypeName System.DirectoryServices.DirectorySearcher
+                        $searcher.Filter = "(&(objectclass=computer)(cn=$TargetComputer*))"
+                        $searcher.SearchRoot = "LDAP://$searchRoot"
+                        # $distinguishedName = $searcher.FindOne().Properties.distinguishedname
+                        # $searcher.Filter = "(member:1.2.840.113556.1.4.1941:=$distinguishedName)"
+
+                        [void]$searcher.PropertiesToLoad.Add("name")
+
+                        $list = [System.Collections.Generic.List[String]]@()
+
+                        $results = $searcher.FindAll()
+                        foreach ($result in $results) {
+                            $resultItem = $result.Properties
+                            [void]$List.add($resultItem.name)
+                        }
+                        $TargetComputer = $list
+
+                    }
+                }
+            }
+            $TargetComputer = $TargetComputer | Where-object { $_ -ne $null }
+            # Safety catch
+            if ($null -eq $TargetComputer) {
+                return
+            }
+        }
+
+
+        ## 3. Create empty results container
+        $results = [system.collections.arraylist]::new()
+    }
+
+
+    ## 1. Make sure no $null or empty values are submitted to the ping test or scriptblock execution.
+    ## 2. Ping the single target computer one time as test before attempting remote session.
+    ## 3. If machine was responseive, run scriptblock to logged in user, info on teams/zoom processes, etc.
+    PROCESS {
+        ForEach ($single_computer in $TargetComputer) {
+
+            ## 1. empty Targetcomputer values will cause errors to display during test-connection / rest of code
+            if ($single_computer) {
+                ## 2. Send one test ping
+                $ping_result = Test-Connection $single_computer -count 1 -Quiet
+                if ($ping_result) {
+                    # Get Computers details and create an object
+                    $dotnetversions = Invoke-Command -ComputerName $single_computer -Scriptblock {
+                        $obj = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -Match '^(?!S)\p{L}' } | Select PSChildName, version
+                        $obj
+                    } | Select PSComputerName, * -ExcludeProperty RunspaceId, PSshowcomputername -ErrorAction SilentlyContinue
+                    
+                    ForEach ($single_dotnet in $dotnetversions) {
+                        $results.add($single_dotnet) | out-null
+                    }
+                }
+                else {
+                    Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: $single_computer is offline." -Foregroundcolor Yellow
+                }
+            }
+        }
+    }
+    ## 1. If there are results - sort them by the hostname (pscomputername) property.
+    ## 2. If the user specified 'n' for outputfile - just output to terminal or gridview.
+    ## 3. Create .csv/.xlsx reports as necessary.
+    END {
+        if ($results) {
+            ## 1. Sort any existing results by computername
+            $results = $results | sort -property pscomputername
+            ## 2. Output to gridview if user didn't choose report output.
+            $results | out-gridview
+        }
+        else {
+            Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: No results to output."
+        }
+        Read-Host "Press enter to return results."
+        return $results
+    }
+}
