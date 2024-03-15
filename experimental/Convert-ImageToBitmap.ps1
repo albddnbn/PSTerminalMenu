ad4f7897956a9ee6ea20c2b317e30f110bc3ce0a
Alex B <105134673+albddnbn@users.noreply.github.com>
2024-03-14 22:10:53 -0400

    Create Convert-ImageToBitmap.ps1
    convert to bit map functionality (wrapper for a couple functions from https://github.com/dwj7738/PowershellModules/blob/master/PSImageTools/).
    Get-Image <image.png> | ConvertTo-Bitmap is one way to use them.
    
    Removed some reports that were created.

---
+++ experimental/Convert-ImageToBitmap.ps1
@@ -0,0 +1,4 @@
+## This function will just be a wrapper that runs functions from here: https://github.com/dwj7738/PowershellModules/blob/master/PSImageTools/Get-Image.ps1
+## To Get-Image with image as target and pipe it into the ConvertTo-Bitmap function.
+
+## I just used it to convert a png to bmp so I could set a background picture with bginfo64.exe / SysInternals, and I've had to do this before so this function will come in handy again.
