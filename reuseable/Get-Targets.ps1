b5ea2e709602983e171893142be08714cdef76b0
Alex B <105134673+albddnbn@users.noreply.github.com>
2024-03-14 23:42:59 -0400

    Update Get-Targets.ps1
    Standalone Get-Targets function in reuseable directory allows for comma-separated list of hostname substrings in addition to comma separated list of hostnames, single hostname, path to hostname text file, or ''/localhost/127.0.0.1 for the local computer.
    
    I have plans to factor this into all functions that use this code section, so theyll all have that added functionality for TargetComputer.

--- reuseable/Get-Targets.ps1
+++ reuseable/Get-Targets.ps1
@@ -1,75 +1,75 @@
 Function Get-Targets {
+    # Standalone 'get target machines' function
+    ## takes COMMA-separated hostname substring lis!
+
     param(
-        [Parameter(
-            Mandatory = $true,
-            ValueFromPipeline = $true,
-            Position = 0
-        )]
         [String[]]$TargetComputer
     )
 
-    if ($null -eq $TargetComputer) {
-        Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Detected pipeline for targetcomputer." -Foregroundcolor Yellow
+    # if ($null -eq $TargetComputer) {
+    #     Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Detected pipeline for targetcomputer." -Foregroundcolor Yellow
+    # }
+    # else {
+
+    ## Do we have to add if targetcomputer eq $null? Will function ever be used like: Get-Targets
+
+    if (($TargetComputer -is [System.Collections.IEnumerable]) -and ($TargetComputer -isnot [string[]])) {
+        $TargetComputer = @('127.0.0.1')
     }
-    else {
-        if (($TargetComputer -is [System.Collections.IEnumerable]) -and ($TargetComputer -isnot [string[]])) {
-            $null
-            ## If it's a string - check for commas, try to get-content, then try to ping.
+    elseif ($TargetComputer -is [string[]]) {
+        if ($TargetComputer -in @('', '127.0.0.1', 'localhost')) {
+            $TargetComputer = @('127.0.0.1')
         }
-        elseif ($TargetComputer -is [string[]]) {
-            if ($TargetComputer -in @('', '127.0.0.1', 'localhost')) {
-                $TargetComputer = @('127.0.0.1')
+        elseif (Test-Path $Targetcomputer -erroraction SilentlyContinue) {
+            $TargetComputer = Get-Content $TargetComputer
+        }
+
+        else {
+            ## Prepare TargetComputer for LDAP query in ForEach loop
+            ## if TargetComputer contains commas - it's either multiple comma separated hostnames, or multiple comma separated hostname substrings - either way LDAP query will verify
+            if ($Targetcomputer -like "*,*") {
+                $TargetComputer = $TargetComputer -split ','
             }
-            elseif (Test-Path $Targetcomputer -erroraction SilentlyContinue) {
-                $TargetComputer = Get-Content $TargetComputer
+            else {
+                $Targetcomputer = @($Targetcomputer)
             }
 
-            else {
-                ## Prepare TargetComputer for LDAP query in ForEach loop
-                ## if TargetComputer contains commas - it's either multiple comma separated hostnames, or multiple comma separated hostname substrings - either way LDAP query will verify
-                if ($Targetcomputer -like "*,*") {
-                    $TargetComputer = $TargetComputer -split ','
+            ## LDAP query each TargetComputer item, create new list / sets back to Targetcomputer when done.
+            $NewTargetComputer = [System.Collections.Arraylist]::new()
+            foreach ($computer in $TargetComputer) {
+                ## CREDITS FOR The code this was adapted from: https://intunedrivemapping.azurewebsites.net/DriveMapping
+                if ([string]::IsNullOrEmpty($env:USERDNSDOMAIN) -and [string]::IsNullOrEmpty($searchRoot)) {
+                    Write-Error "LDAP query `$env:USERDNSDOMAIN is not available!"
+                    Write-Warning "You can override your AD Domain in the `$overrideUserDnsDomain variable"
                 }
                 else {
-                    $Targetcomputer = @($Targetcomputer)
-                }
 
-                ## LDAP query each TargetComputer item, create new list / sets back to Targetcomputer when done.
-                $NewTargetComputer = [System.Collections.Generic.List[string]]
-                foreach ($computer in $TargetComputer) {
-                    ## CREDITS FOR The code this was adapted from: https://intunedrivemapping.azurewebsites.net/DriveMapping
-                    if ([string]::IsNullOrEmpty($env:USERDNSDOMAIN) -and [string]::IsNullOrEmpty($searchRoot)) {
-                        Write-Error "LDAP query `$env:USERDNSDOMAIN is not available!"
-                        Write-Warning "You can override your AD Domain in the `$overrideUserDnsDomain variable"
+                    # if no domain specified fallback to PowerShell environment variable
+                    if ([string]::IsNullOrEmpty($searchRoot)) {
+                        $searchRoot = $env:USERDNSDOMAIN
                     }
-                    else {
-
-                        # if no domain specified fallback to PowerShell environment variable
-                        if ([string]::IsNullOrEmpty($searchRoot)) {
-                            $searchRoot = $env:USERDNSDOMAIN
-                        }
-                        $searcher = New-Object -TypeName System.DirectoryServices.DirectorySearcher
-                        $searcher.Filter = "(&(objectclass=computer)(cn=$computer*))"
-                        $searcher.SearchRoot = "LDAP://$searchRoot"
-                        [void]$searcher.PropertiesToLoad.Add("name")
-                        $list = [System.Collections.Generic.List[String]]@()
-                        $results = $searcher.FindAll()
-                        foreach ($result in $results) {
-                            $resultItem = $result.Properties
-                            [void]$List.add($resultItem.name)
-                        }
-                        $NewTargetComputer += $list
+                    $searcher = New-Object -TypeName System.DirectoryServices.DirectorySearcher
+                    $searcher.Filter = "(&(objectclass=computer)(cn=$computer*))"
+                    $searcher.SearchRoot = "LDAP://$searchRoot"
+                    [void]$searcher.PropertiesToLoad.Add("name")
+                    $list = [System.Collections.Generic.List[String]]@()
+                    $results = $searcher.FindAll()
+                    foreach ($result in $results) {
+                        $resultItem = $result.Properties
+                        [void]$List.add($resultItem.name)
                     }
+                    $NewTargetComputer += $list
                 }
-                $TargetComputer = $NewTargetComputer
             }
+            $TargetComputer = $NewTargetComputer
+        }
 
-        }
-        $TargetComputer = $TargetComputer | Where-object { $_ -ne $null }
-        # Safety catch
-        if ($null -eq $TargetComputer) {
-            return
-        }
     }
+    $TargetComputer = $TargetComputer | Where-object { $_ -ne $null }
+    # Safety catch
+    if ($null -eq $TargetComputer) {
+        return
+    }
+    # }
     return $TargetComputer
 }
