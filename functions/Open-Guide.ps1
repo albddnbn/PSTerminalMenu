19723057b1d17a67def5e5c28d3a7023c21314f3
Alex B. <105134673+albddnbn@users.noreply.github.com>
2024-09-16 16:24:53 -0400

    Update Open-Guide.ps1

--- functions/Open-Guide.ps1
+++ functions/Open-Guide.ps1
@@ -13,12 +13,18 @@ function Open-Guide {
         Project Site: https://github.com/albddnbn/PSTerminalMenu
     #>
 
-    $PSTERMINALMENU_WIKI_URL = "https://github.com/albddnbn/PSTerminalMenu/wiki"
+    $HELP_URL = "https://github.com/albddnbn/PSTerminalMenu/wiki"
+    Write-Host "Attempting to open " -nonewline
+    Write-Host "$HELP_URL" -Foregroundcolor Yellow -NoNewline
+    Write-Host " in default browser."
+
 
     try {
-        Start-Process "https://github.com/albddnbn/PSTerminalMenu/wiki"
+        $chrome_exe = Get-ChildItem -Path "C:\Program Files\Google\Chrome\Application" -Filter "chrome.exe" -File -ErrorAction SilentlyContinue
+        Start-Process "$($chrome_exe.fullname)" -argumentlist "$HELP_URL"
     }
     catch {
-        Write-Host "Failed to open the guide. Please visit $PSTERMINALMENU_WIKI_URL" -ForegroundColor Red
+        Start-Process "msedge.exe" -argumentlist "$HELP_URL"
     }
+    Read-Host "Press enter to continue."
 }
